8331d5f139938ecb7de2ca1d6cabb62845038740
deni1804 <denizulkarnaen223gmail.com>
2021-10-28 02:26:49 +0700

    frist commit

---
+++ application/views/templates/templates-user/footer.php
@@ -0,0 +1,20 @@
+</div>
+<script src="asset/js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="asset/js/jquery-3.3.1.slim.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+<script src="<?= base_url(); ?>assets/user/js/bootstrap.js"></script>
+
+<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- core plugin javascript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Costem scripts for all pages-->
+<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+<script>
+    $('.alert').alert().delay(3000).slideup('slow');
+</script>
+</body>
+
+</html>
